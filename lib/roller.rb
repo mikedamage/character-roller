9f62f80685368b7cba3a86a33c66660e4304d502
Mike Green <mike.is.green@gmail.com>
2013-12-09 09:36:43 -0500

    Added quick method to roll all 6 ability scores at once

--- lib/roller.rb
+++ lib/roller.rb
@@ -42,6 +42,13 @@ class Roller < Thor
     say results.reduce :+
   end
 
+  desc 'ability_scores', 'Rolls 6 ability scores'
+  def ability_scores
+    6.times do
+      ability_score
+    end
+  end
+
   no_tasks do
     def roll_one_die(sides, ignore_ones = false)
       result = 1 + Random.rand(sides)
